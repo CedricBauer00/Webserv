1b4825f72f6b7cf2a7baa9c7a3432483dd911c4e
Cedric Bauer <cbauer@3-B-3.42heilbronn.de>
2026-04-08 15:12:27 +0200

    built a mini server, that can respond http, but only 1 page

--- src/eventloop.cpp
+++ src/eventloop.cpp
@@ -47,9 +47,48 @@ httpServer::~httpServer()
     std::cout << "Server destroyed" << std::endl;
 }
 
+void sigchld_handler(int s)
+{
+    (void)s; // quiet unused variable warning
+
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
+
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+
+    errno = saved_errno;
+}
+
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+void    print_addrinfo( struct addrinfo *p, int it, char *s )
+{
+    std::cout << "\n--- iteration: " << it << "---" << std::endl;
+    std::cout << "ai_flags: " << p->ai_flags << std::endl;
+    std::cout << "ai_family: " << (p->ai_family == AF_INET ? "AF_INET (4)" : p->ai_family == AF_INET6 ? "AF_INET6 (6)" : "UNKNOWN" ) << std::endl;
+    std::cout << "ai_socktype: " << (p->ai_socktype == SOCK_STREAM ? "SOCK_STREAM" : "UNKNOWN" ) << std::endl;
+    std::cout << "ai_protocol: " << p->ai_protocol << std::endl;
+    std::cout << "ai_addrlen: " << p->ai_addrlen << std::endl;
+
+    if ( p->ai_canonname )
+        std::cout << "ai_canonname: " << p->ai_canonname << std::endl;
+
+    inet_ntop( p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof( s ) );
+    std::cout << "Address: " << s << std::endl;
+}
+
 int httpServer::run()
 {
     int rv, sockfd, yes=1;
+    struct sigaction sa;
+    char s[INET6_ADDRSTRLEN];
 
     std::cout << "Running webserver" << std::endl;
 
@@ -65,9 +104,12 @@ int httpServer::run()
         return 1;
     }
 
+    int it = 0;
     for ( p = servinfo; p != NULL; p = p->ai_next )
     {
-        if ( ( sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol ) ) != -1 )
+        
+        print_addrinfo(p, it++, s);
+        if ( ( sockfd = socket( p->ai_family, p->ai_socktype, p->ai_protocol ) ) == -1 )
         {
             perror( "server: socket" );
             continue ;
@@ -85,6 +127,9 @@ int httpServer::run()
             perror( "server: bind" );
             continue ;
         }
+
+        std::cout << "printing family: " << (p->ai_family == AF_INET ? "AF_INET" : "AF_INET6") << std::endl;
+
         break ;
     }
 
@@ -102,7 +147,46 @@ int httpServer::run()
         exit( 1 );
     }
 
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
 
+    std::cout << "Server wating for connection...\n" << std::endl;
+
+    socklen_t sin_size;
+    struct sockaddr_storage their_addr;
+    int new_fd;
+    const char* response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 44\r\n\r\n<html><body>Hello, World!</body></html>";
+
+    while(1) {  // main accept() loop
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr,
+            &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
+
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+        printf("server: got connection from %s\n", s);
+
+        if (!fork()) { // this is the child process
+            close(sockfd); // child doesn't need the listener
+            if (send(new_fd, response, strlen(response), 0) == -1)
+                perror("send");
+            close(new_fd);
+            exit(0);
+        }
+        close(new_fd);  // parent doesn't need this
+    }
+
+    return 0; 
 
     return 0;
 }
