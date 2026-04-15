8635afd5a732fd4e2c66b1b97557454c72a6a2e4
CedricBauer00 <bauercedric91@gmail.com>
2026-04-15 08:39:20 +0200

    MERGE: merging from macbook

--- inc/httpparser.hpp
+++ inc/httpparser.hpp
@@ -1,13 +1,36 @@
 #pragma once
 
+#include <unistd.h>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <sys/epoll.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#define RED  "\033[31m"
+#define ELEC_RED "\033[38;2;255;20;20m"
+#define BLUE    "\033[34m"
+#define GREEN  "\033[32m"
+#define ORANGE  "\033[38;2;255;120;0m"
+#define RESET  "\033[0m"
+
 class HttpParsing
 {
     private:
-
+        std::string _request;
     public:
-        HttpParsing();
-        ~HttpParsing();
+        HttpParser();
+        ~HttpParser();
+        int parse();
 
+        std::string getRequest();
         
-
-}
+//
+};
--- src/HttpParser.cpp
+++ src/HttpParser.cpp
@@ -1,2 +1,21 @@
 #include "../inc/httpparser.hpp"
 
+HttpParsing::HttpParser( std::string request ) : _result(request )
+{
+    std::cout << "HttpParsing" << std::endl;
+}
+
+HttpParsing::~HttpParser() : _result()
+{
+    std::cout << "HttpParsing" << std::endl;
+}
+
+int HttpParsing::parse()
+{
+
+}
+
+std::string HttpParsing::getRequest()
+{
+    return _request;
+}
