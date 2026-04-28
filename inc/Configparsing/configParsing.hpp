9bc49f9fd5bf476a60e6db81af86df0720e9e533
Vishnudev Ramachandra <vramacha@2-C-1.42heilbronn.de>
2026-04-28 18:19:45 +0200

    FIX[configparsing]: workable struct for config made

--- inc/Configparsing/configParsing.hpp
+++ inc/Configparsing/configParsing.hpp
@@ -7,20 +7,46 @@
 #define LISTEN (1 << 0)
 #define DEFAULT_SERVER (1 << 1)
 
+typedef struct s_webserrv_connection {
+	int									fd;
+	std::string							ip;
+	unsigned int						port;
+} t_webserv_connection;
+
+typedef struct s_webserv_http_request {
+	t_webserv_connection*					conn;
+	std::string								method, uri, httpVersion;
+	std::vector<std::pair<std::string, std::string>> headers;
+}	t_webserv_http_request;
+
+typedef int (*t_webserv_handler)(t_webserv_http_request* r);
+
+typedef struct {
+	void**	main_conf;
+	void**	srv_conf;
+	void**	loc_conf;
+}	t_webserv_conf_ctx;
+
+typedef struct {
+	std::string		domain;
+	unsigned int	port;
+}	t_webserv_addr;
+
 typedef std::chrono::milliseconds	t_webserv_msec;
 
-typedef t_webserv_srv_config;
+typedef struct s_webserv_http_conf	t_webserv_http_conf;
+typedef struct s_webserv_srv_conf	t_webserv_srv_conf;
+typedef struct s_webserv_loc_conf	t_webserv_loc_conf;
 
 typedef struct {
-	std::vector<t_webserv_srv_config>   servers; // virtual servers
-}	t_webserv_http_config;
-
-typedef	t_webserv_loc_config;
+	std::vector<t_webserv_srv_conf>   servers; // virtual servers
+	std::vector<std::pair<t_webserv_addr, t_webserv_phase_engine>> ph;
+}	t_webserv_http_conf;
 
 typedef struct {
-	t_webserv_http_config				*httpConfig; // http configuration
+	t_webserv_conf_ctx*					ctx;
+	t_webserv_phase						phases[10];
 	std::vector<std::string>			serverNames; // virtual server name entries
-	std::vector<t_webserv_loc_config>	locations; // location blocks
 	std::string							filename, serverName;
 	unsigned int						lineNum;
 	size_t								numReqExpected; // number of simultaneous requests expected
@@ -28,4 +54,5 @@ typedef struct {
 	bool								ignore_invalid_headers{true}, \
 	merge_slashes{true}, underscore_is_valid{false};
 	unsigned int						flags{0};
-}	t_webserv_srv_config;
+}	t_webserv_srv_conf;
+
